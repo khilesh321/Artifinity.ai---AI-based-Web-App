e443e0122a7086e2314fbb9ce67f3ba97109d7ef
Khilesh Jawale <khilujawale321@gmail.com>
2025-07-15 21:58:47 +0530

    feat: enhance UI interactions with hover effects on buttons and cards

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
 
       <div className="flex flex-wrap justify-center gap-4 mt-2 text-sm max-sm:text-xs">
         <button onClick={() => navigate('/ai')} className="bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer">Explore AI Tools</button>
-        <button className="bg-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer">Watch demo</button>
+        <button className="border-1 border-black/70 px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer">Watch demo</button>
       </div>
 
       <div className="flex items-center gap-4 mt-6 mx-auto text-gray-600">
