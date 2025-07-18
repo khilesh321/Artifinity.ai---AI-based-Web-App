2983d6a7cf042867f4fac541394dea75528c4a75
Khilesh Jawale <khilujawale321@gmail.com>
2025-07-18 18:11:53 +0530

    feat(frontend): implement Remove Background feature with image upload functionality

--- frontend/src/pages/RemoveBackground.jsx
+++ frontend/src/pages/RemoveBackground.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import { Upload, Sparkles, Eraser } from "lucide-react"
+import { useState } from "react"
 
 function RemoveBackground() {
+  const [image, setImage] = useState(null);
+
   return (
-    <div>RemoveBackground</div>
+    <div className="h-full overscroll-y p-6 flex items-start flex-wrap gap-4 text-slate-70">
+      {/* left column */}
+      <form onSubmit={(e) => {
+          e.preventDefault();
+          // Handle form submission
+        }} className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#EA580C]"></Sparkles>
+          <h1 className="text-xl font-semibold">Remove Background</h1>
+        </div>
+        
+        <p className="mt-6 text-sm font-medium">Upload Image</p>
+
+        <div className="mt-2 flex justify-center items-center w-full">
+          <label htmlFor="dropzone-file" className="flex flex-col justify-center items-center w-full h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer hover:bg-gray-100">
+            <div className="flex flex-col justify-center items-center pt-5 pb-6">
+              <Upload className="w-10 h-10 mb-3 text-gray-400" />
+              <p className="mb-2 text-sm text-gray-500"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+              <p className="text-xs text-gray-500">PNG, JPG, GIF and other image formats</p>
+            </div>
+            <input id="dropzone-file" type="file" className="hidden" onChange={(e) => setImage(e.target.files[0])} />
+          </label>
+        </div>
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#F97316] to-[#EA580C] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Eraser className="w-5"/>
+          Remove Background
+        </button>
+      </form>
+      {/* right column */}
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200 flex flex-col min-h-96 max-h-[600px]">
+        <div className="flex items-center gap-3">
+          <Eraser className="w-6 text-[#EA580C]"></Eraser>
+          <h1 className="text-xl font-semibold">Result</h1>
+        </div>
+
+        <div className="flex flex-1 justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Upload className="w-9 h-9 text-gray-400" />
+            <p>Upload an image to see the result</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
